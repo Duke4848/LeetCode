8ee908b14d6349d1cf5b3a3e35a822855e60a6b7
Robert Kilar <robert.kilar@bp.com>
2024-08-25 20:38:45 +0200

    x

---
+++ 278FirstBadVersion/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _278FirstBadVersion
+{
+    public class Solution
+    {
+        public int FirstBadVersion(int n)
+        {
+            if (n == 1) return n;
+            int l = 1, r = n;
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if (!IsBadVersion(mid))
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+
+            return l;
+        }
+
+        public bool IsBadVersion(int v)
+        {
+            bool[] t = { false, false, false, true, true };
+            return t[v - 1];
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().FirstBadVersion(5));
+        }
+    }
+}
---
+++ 938Range Sum Bst/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _938Range_Sum_Bst
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root == null) return 0;
+            var ret = 0;
+            if (root.val >= low && root.val <= high)
+            {
+                ret = root.val;
+            }
+            return ret + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        }
+
+        //public int RangeSumBST(TreeNode root, int low, int high, int sum)
+        //{
+        //    if (root == null) return sum;
+        //    if (root.val <= low && root.val >= high)
+        //    {
+        //        sum += root.val;
+        //    }
+        //    RangeSumBST(root.left, low, high, sum);
+        //    RangeSumBST(root.right, low, high, sum);
+
+        //}
+
+
+    }
+}
